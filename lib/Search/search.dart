2e550a809e09acf34cf87819e6ee149540687a7e
sayalicse <spsayalipatil2003@gmail.com>
2025-08-08 17:26:29 +0530

    updated search page along with text by feature/personA

--- lib/Search/search.dart
+++ lib/Search/search.dart
@@ -1,9 +1,53 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget{
+class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+      backgroundColor: Colors.black, // background to see white search bar
+      appBar: AppBar(
+        title: Text("Search"),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Start browsing',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10), // Spacing between text and search bar
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.search, color: Colors.grey),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Text(
+                      "What do you want to listen to?",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
+
